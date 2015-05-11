71dfd093f292f19462b9b521a5cac7376ed8da05
취업반PM <PM@ITBANK28-PC>
2015-05-11 21:45:00 +0900

    lunching..

---
+++ jse/src/cmm02/op/No01_BlockDemo.java
@@ -0,0 +1,16 @@
+package cmm02.op;
+
+public class No01_BlockDemo {
+
+	public static void main(String[] args) {
+		boolean condition = true;
+		
+		if(condition){
+			System.out.println("Âü");
+		}else{
+			System.out.println("°ÅÁþ");
+		}
+
+	}
+
+}
